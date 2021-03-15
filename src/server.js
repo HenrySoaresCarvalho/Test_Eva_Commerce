87f91e1e6baa48132492dabae93244aeea8a4213
HenrySoaresCarvalho <henrysoarescarvalho@gmail.com>
2021-03-15 19:02:50 -0300

    Add app server

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+const routes = require("./routes")
+require('./database')
+
+app.use(express.json())
+
+app.use(routes)
+
+app.listen(3000,()=>{
+    console.log("Server Started!")
+})
