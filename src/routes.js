9ea3c9e9d92065d9829de3d1ad5e708aa7e20d13
HenrySoaresCarvalho <henrysoarescarvalho@gmail.com>
2021-03-15 20:21:37 -0300

    jwt test route added

--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,6 @@ const routes = express.Router()
 
 routes.post("/user/register",UserController.store)
 routes.post("/user/login",UserController.user_login)
+routes.post("/test",UserController.authenticator,UserController.test)
 
 module.exports = routes
