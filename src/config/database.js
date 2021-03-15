faf792b8c7ec38c2b70c01f19ec5042b0552f514
HenrySoaresCarvalho <henrysoarescarvalho@gmail.com>
2021-03-15 19:01:52 -0300

    DataBase Configs

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+    dialect: "mysql",
+    host: "localhost",
+    //aqui vai o nome do usuario do seu banco
+    username: "root",
+    //aqui vai a senha do seu banco
+    password: "deidara123",
+    database: "eva_commerce",
+    define:{
+        timestamps: true,
+        underscored:true
+    }
+}
