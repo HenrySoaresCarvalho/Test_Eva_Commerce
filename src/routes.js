5a90fb9ec0ef633c47de3d77f5625c95730a8433
HenrySoaresCarvalho <henrysoarescarvalho@gmail.com>
2021-03-15 21:32:46 -0300

    added product register route

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const ProductController = require('./controllers/ProductController')
 const UserController = require('./controllers/UserController')
 
 const routes = express.Router()
 
 routes.post("/user/register",UserController.store)
 routes.post("/user/login",UserController.user_login)
-routes.post("/test",UserController.authenticator,UserController.test)
+
+routes.post("/product/register",UserController.authenticator,ProductController.store)
 
 module.exports = routes
