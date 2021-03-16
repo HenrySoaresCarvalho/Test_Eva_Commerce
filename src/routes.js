2cac90ba5121b736253846bcc7a2a72c03a18339
HenrySoaresCarvalho <henrysoarescarvalho@gmail.com>
2021-03-15 22:19:54 -0300

    added list routes

--- src/routes.js
+++ src/routes.js
@@ -8,5 +8,7 @@ routes.post("/user/register",UserController.store)
 routes.post("/user/login",UserController.user_login)
 
 routes.post("/product/register",UserController.authenticator,ProductController.store)
+routes.get("/product",ProductController.get_all_products)
+routes.get("/product/:id",ProductController.get_product_by_id)
 
 module.exports = routes
