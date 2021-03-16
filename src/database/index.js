9f069234eef6612005ca51fa14921d1a21462deb
HenrySoaresCarvalho <henrysoarescarvalho@gmail.com>
2021-03-15 21:33:52 -0300

    added product db configs

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,12 @@
 const Sequelize = require('sequelize')
 const config = require("../config/database")
+const Product = require('../models/Product')
 const User = require('../models/User') 
 
 const connection = new Sequelize(config)
 
 User.init(connection)
+Product.init(connection)
+Product.associate(connection.models)
 
 module.exports = connection;
